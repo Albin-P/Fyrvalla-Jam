283296ca78a0fae30fceab897b0be0e4f07716a1
Albin <albin.petersson@jgy.se>
2022-02-06 11:55:53 +0100

    gjort ett mål

--- Assets/FinishLine.cs
+++ Assets/FinishLine.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
+        if(collision.tag == "Player")
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            Debug.Log("Finish");
+           // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }
