66b5064dfdec91b5fb55cb9617352b2217006f14
Albin <albin.petersson@jgy.se>
2022-02-24 15:26:58 +0100

    fixat ett loot script (behöver jobba mer på)

--- Assets/Scripts/Misc Scripts/ItemCollect.cs
+++ Assets/Scripts/Misc Scripts/ItemCollect.cs
@@ -2,21 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoldDrop : MonoBehaviour
+public class ItemCollect : MonoBehaviour
 {
+
     public Enemy enemy;
+
     // Start is called before the first frame update
     void Start()
     {
-        this.enemy = GetComponent<Enemy>();
+        this.GetComponent<Player>();
+        this.GetComponent<Enemy>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(enemy.health <= 0)
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.gameObject.tag == "Player")
         {
-
+            
         }
     }
 }
